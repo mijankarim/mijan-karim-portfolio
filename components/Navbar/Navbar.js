11ccfb404263a1b4c32c650ec9d7b11b99f2d075
Mijan Karim <mkatbd@gmail.com>
2021-05-02 09:42:12 +0600

    added route

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Link from 'next/link';
 import styles from './Navbar.module.css';
 
 const Navbar = () => {
     return (
         <div className={styles.container}>
-            Navbar
+           <nav>
+               <ul>
+                   <li><Link href="/about">About</Link></li>
+                   <li><Link href="/contact">Contact</Link></li>
+               </ul>
+           </nav>
+
         </div>
     )
 }
